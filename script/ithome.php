ff7364f310ec2902bd7c442871fb9ccb9d6eb6b0
deq <630906612@qq.com>
2024-05-28 11:20:27 +0800

    fix: ithome script

--- script/ithome.php
+++ script/ithome.php
@@ -3,32 +3,34 @@ date_default_timezone_set('Asia/Shanghai');
 header("Content-type:application/json; charset=utf-8");
 
 function ithome()
- {
- 	$html = file_get_contents("https://m.ithome.com/rankm/");
-	$pattern = '/<p class="plc-title">(.*?)<\/p>.*?<a href="(.*?)"/s';
+{
+	$html = file_get_contents("https://m.ithome.com/rankm/");
+	// $pattern = '/<p class="plc-title">(.*?)<\/p>.*?<a href="(.*?)"/s';
+	// preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
+
+	$pattern = '/<a href="(.*?)".*?<p class="plc-title">(.*?)<\/p>/s';
 	preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
-	
+
 	$results = [];
-	
 	foreach ($matches as $index => $match) {
-	    $title = trim($match[1]);
-	    $url = $match[2];
-	
-	    // 将标题和链接添加到结果数组
-	    $results[] = [
-	    'index' => $index+1,
-	        'title' => $title,
-	        'url' => $url,
-	    ];
+		$title = trim($match[1]);
+		$url = $match[2];
+
+		// 将标题和链接添加到结果数组
+		$results[] = [
+			'index' => $index + 1,
+			'title' => $title,
+			'url' => $url,
+		];
+	}
+	return [
+		'success' => true,
+		'title' => 'IT之家',
+		'subtitle' => '热榜',
+		'update_time' => date('Y-m-d H:i:s', time()),
+		'data' => $results
+	];
 }
- 	return [
-      'success' => true,
-      'title' => 'IT之家',
-      'subtitle' => '热榜',
-      'update_time' => date('Y-m-d H:i:s', time()),
-      'data' => $results
-    ];
- }
 
 if (basename(__FILE__) === basename($_SERVER['PHP_SELF'])) {
 	$_res = ithome();
@@ -36,4 +38,3 @@ if (basename(__FILE__) === basename($_SERVER['PHP_SELF'])) {
 	$json = str_replace('\/', '/', $json);
 	echo $json;
 }
-?>
